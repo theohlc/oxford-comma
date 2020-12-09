f52ec38d5be9693c4334a04637eadfccdad3c00f
theo <teddyhoover@gmail.com>
2020-12-09 15:05:04 -0600

    commit tree 6b399e9d9ad3f1c3c9782f65e99908a0a88f421f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        "#{array[0]} and #{array[1]}"
+     else
+        last = array.pop
+        "#{array.join(", ")}, and #{last}"
+     end
 end
